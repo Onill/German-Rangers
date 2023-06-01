1103cea6282747f20143bc5da92ca65940222c1c
Ganvai <mail@jan-haak.com>
2023-06-01 08:15:08 +0200

    Comment Fix

--- GR_MM_Template_v3.VR/config.hpp
+++ GR_MM_Template_v3.VR/config.hpp
@@ -1,7 +1,7 @@
 //German Rangers Mission Feature Config Variables
 useDBMapMarkers 	= false;		//Sets if MapMarkers from the DB are loaded for this mission
-lima				= true;			//Sets if a Lima Supply Point is for the Group Lima and Kilo available (Needs adjusted supply boxes in Lima folder for the Fraktion)
-missionstartedfeat	= true;			//Sets if the Mission Started Feature is available. When the Feature is "true", all Players have the GR GUI everywhere available until the Mission is started by Zeus
+lima				= true;			//Sets if a Lima Supply Point is available for all Players in Groups Lima and Kilo(Needs adjusted supply boxes in Lima folder for the Fraktion)
+missionstartedfeat	= true;			//Sets if the "Mission Started"-Feature is available. When the Feature is "true", all Players have the GR GUI everywhere available until the Mission is started by Zeus
 									//After the Mission was started, every JIP Player will be spawned in the Base, defined in initPlayerLocal.sqf. 
 									//This is used for Missions that start in field. Needs the Mission Start Feature from Zeus!
 
